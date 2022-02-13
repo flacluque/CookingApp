0e44ca5e47d2c30b0d18ef0dccc5e780cc445cff
flacluque <flav64.lacluque@gmail.com>
2022-02-13 21:56:22 +0100

    changement du lien vers l'image

--- component/RecipePage.js
+++ component/RecipePage.js
@@ -91,7 +91,7 @@ export function RecipePage({ route }) {
       <Image
         style={[{ height: 300, resizeMode: "cover" }]}
         source={{
-          uri: "https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8.jpg",
+          uri: recipe.imgSrc,
         }}
       />
       <TitleAndDescription
